9278c273eef95ff38aaff17b76e9cbf3b01e2bd7
a-paudel <ankur@paudel.me>
2023-11-03 23:12:34 +0000

    rename snippets file

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -5,6 +5,7 @@ return {
   dependencies = { "rafamadriz/friendly-snippets" },
   opts = function(_, opts)
     require("luasnip").filetype_extend("python", { "django" })
+    require("luasnip").filetype_extend("templ", { "html" })
     require("luasnip.loaders.from_vscode").lazy_load()
   end,
 }
