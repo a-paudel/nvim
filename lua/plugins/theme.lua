63eec67126c3b46004e7e5a38285adf9320ab12d
a-paudel <ankur@paudel.me>
2023-11-04 11:10:09 +0000

    Change theme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "catppuccin-latte",
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
